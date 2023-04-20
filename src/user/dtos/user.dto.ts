0b1d536d4d1401e116d2c55433ea6b3c49480afc
Gabril D. Padua <gabriel.d.padua21@gmail.com>
2023-04-20 04:01:21 -0300

    feat: ✨ controller create user

---
+++ src/user/dtos/user.dto.ts
@@ -0,0 +1,7 @@
+export interface CreateUserDto {
+    user: string;
+    email: string;
+    phone: string;
+    cpf: string;
+    password: string;
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+
+    @Post()
+    async createUser(@Body() newUser: CreateUserDto) {
+        return {
+            ...newUser,
+            password: undefined
+        }
+    }
+
+    @Get()
+    async getUser() {
+        return JSON.stringify({
+            user: "teste",
+            password: "teste"
+        })
+    }
+
+}
